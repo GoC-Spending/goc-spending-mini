1f3ed06f31f5ca0970721e185d276844f3b7122c
Lucas Cherkewski <lucas@ecustom.ca>
2017-06-22 21:32:20 -0400

    remove Agr FileParser

--- src/FileParser.php
+++ src/FileParser.php
@@ -6,57 +6,6 @@ use \XPathSelector\Selector;
 
 class FileParser {
 
-    // Fun with Regular Expressions
-    // ([A-Z])\w+
-    public static function agr($html) {
-
-        $values = [];
-        $keyToLabel = [
-            'vendorName' => 'Vendor Name:',
-            'referenceNumber' => 'Reference Number:',
-            'contractDate' => 'Contract Date:',
-            'description' => 'Description Of Work:',
-            'contractPeriodStart' => '',
-            'contractPeriodEnd' => '',
-            'contractPeriodRange' => 'Contract Period:',
-            'deliveryDate' => '',
-            'originalValue' => 'Original Contract Value:',
-            'contractValue' => 'Contract Value:',
-            'comments' => 'Comments:',
-        ];
-        $labelToKey = array_flip($keyToLabel);
-
-        $matches = [];
-        $pattern = '/<th scope="row">([\wÀ-ÿ@$#%^&+\*\-.\'(),;:<\/>\s]*)<\/th><td>([\wÀ-ÿ@$#%^&+\*\-.\'(),;:<\/>\s]*)<\/td>/';
-
-        preg_match_all($pattern, $html, $matches, PREG_SET_ORDER);
-
-        // var_dump($matches);
-
-        foreach($matches as $match) {
-
-            $label = $match[1];
-            $value = $match[2];
-
-            if(array_key_exists($label, $labelToKey)) {
-
-                $values[$labelToKey[$label]] = Helpers::cleanHtmlValue($value);
-
-            }
-
-        }
-
-        // Change the "to" range into start and end values:
-        if(isset($values['contractPeriodRange']) && $values['contractPeriodRange']) {
-            $split = explode(' to ', $values['contractPeriodRange']);
-            $values['contractPeriodStart'] = $split[0];
-            $values['contractPeriodEnd'] = $split[1];
-        }
-
-        return $values;
-
-    }
-
     public static function csa($html) {
 
         // Just get the table in the middle:
